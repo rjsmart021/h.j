50e12015a652d6350eb660858a514e0eba3e6797
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-09 17:16:14 -0600

    Create j.js

---
+++ j.js
@@ -0,0 +1,29 @@
+//Task 1: Declare variables to represent student information such as name, age, grade, and attendance status.
+let studentName2 = "John Doe"; // Name of the student
+let studentAge2 = 16; // Age of the student
+let studentGrade2 = "10th Grade"; // Grade of the student
+let isPresent2 = true; // Attendance status (true for present, false for absent)
+//Task 2: Assign sample values
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10;
+isPresent = true;
+//Task 3: Assign sample values
+
+console.log("studentName:", studentName)
+console.log("studentAge:", studentAge)
+console.log("studentGrade:", studentGrade)
+console.log("isPresent:", isPresent)
+//Exploring JavaScript Operators
+//Task 1: Declare variables to store two numeric values for performing arithmetic operations.
+var num1 = 5;
+var num2 = 14;
+var num3 = 12;
+//Task 2: Assign sample numeric values to the variables declared in Task 1.
+let  num1 = 5;
+let num2 = 14;
+let num3 = 12;
+//Task 3: Perform arithmetic operations on your numeric values using various operators and display the results.
+let totalz = num3 + num2 
+let total2 = num2 - num3
+let total = num2 - num1
